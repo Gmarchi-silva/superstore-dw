4904ed2d4b9674a623a803aaa1666c1f4662bc2f
igortavares2@hotmail.com <igortavares2@hotmail.com>
2023-03-04 20:12:04 +0000

    atualização das tabelas dim_customer (dw e snapshot)

--- superstore_dw/models/dim_customer.sql
+++ superstore_dw/models/dim_customer.sql
@@ -1,6 +1,15 @@
+
+
 select
+    row_number() over (order by c.customer_id) as sk_customer,
     c.customer_id,
     c.name as customer_name,
-    s.segment    
+    s.segment,
+    spc.valid_from,
+    spc.valid_to,
+    spc.created_at,
+    spc.last_updated_at
+
 from {{ source("norm", "t_customer") }} c
     join {{ source("norm", "t_segment") }} s on c.segment_id = s.segment_id
+    join {{ source("snapshots", "sp_dim_customer") }} spc on c.customer_id = spc.customer_id
